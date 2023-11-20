d518be3b43c7cce313551f16e2b7c3c3844b2ec0
BlackComposit <148697832+BlackComposit@users.noreply.github.com>
2023-11-21 01:38:24 +0300

    Создана фабрика вопросов. Модели вопросов вынесены в отдельные файлы.

--- MovieQuiz/Models/QuizStepViewModel.swift
+++ MovieQuiz/Models/QuizStepViewModel.swift
@@ -5,4 +5,10 @@
 //  Created by Дмитрий Ивашинин on 21.11.2023.
 //
 
-import Foundation
+import UIKit
+
+struct QuizStepViewModel {
+  let image: UIImage
+  let question: String
+  let questionNumber: String
+}
