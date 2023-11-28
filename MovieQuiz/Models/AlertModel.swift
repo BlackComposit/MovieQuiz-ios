9237fc542053292ebbf2a36406576f8319fde864
BlackComposit <148697832+BlackComposit@users.noreply.github.com>
2023-11-29 01:26:01 +0300

    Вынесение логики алерта в AlertPresenter.

---
+++ MovieQuiz/Models/AlertModel.swift
@@ -0,0 +1,8 @@
+//
+//  AlertModel.swift
+//  MovieQuiz
+//
+//  Created by Дмитрий Ивашинин on 28.11.2023.
+//
+
+import Foundation
---
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  AlertPresenter.swift
+//  MovieQuiz
+//
+//  Created by Дмитрий Ивашинин on 28.11.2023.
+//
+
+import Foundation
