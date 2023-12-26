b5556e8939e3a88f9f7636bdd88af40a77b5e6c6
BlackComposit <148697832+BlackComposit@users.noreply.github.com>
2023-12-27 00:36:42 +0300

    Реализация сетевого клиента и создание загрузчика в модели

---
+++ MovieQuiz/Models/MostPopularMovies.swift
@@ -0,0 +1,8 @@
+//
+//  MostPopularMovies.swift
+//  MovieQuiz
+//
+//  Created by Дмитрий Ивашинин on 26.12.2023.
+//
+
+import Foundation
---
+++ MovieQuiz/Services/NetworkClient.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkClient.swift
+//  MovieQuiz
+//
+//  Created by Дмитрий Ивашинин on 26.12.2023.
+//
+
+import Foundation
